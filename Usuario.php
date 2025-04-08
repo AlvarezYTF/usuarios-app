071fe9b1f6d1cfca661c06409d742a77c709830d
Camacho <cristiancamachocamacho406@gmail.com>
2025-04-08 00:22:11 -0500

    Refactor Usuario class constructor and Crear method for improved clarity and consistency

--- Usuario.php
+++ Usuario.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 require_once 'Database.php';
-class Usuario extends Database { 
-    public $id;
+class Usuario extends Database
+{
     public $primerNombre;
     public $segundoNombre;
     public $primerApellido;
@@ -9,32 +9,32 @@ class Usuario extends Database {
     public $fecha_nacimiento;
     public $telefono;
 
-    public function __construct($id, $primerNombre,$segundoNombre,$primerApellido,$segundoApellido, $fecha_nacimiento, $telefono) {
-        $this->id = $id;
+    public function __construct($primerNombre, $segundoNombre, $primerApellido, $segundoApellido, $fecha_nacimiento, $telefono)
+    {
+
         $this->primerNombre = $primerNombre;
-        $this->segundoNombre = $segundoNombre;    
-        $this->primerApellido=$primerApellido;
-        $this->segundoApellido=$segundoApellido;    
+        $this->segundoNombre = $segundoNombre;
+        $this->primerApellido = $primerApellido;
+        $this->segundoApellido = $segundoApellido;
         $this->fecha_nacimiento = $fecha_nacimiento;
         $this->telefono = $telefono;
     }
 
-    public function Crear($id, $primerNombre, $segundoNombre, $primerApellido, $segundoApellido, $fecha_nacimiento, $telefono){
-        try {            
-            $sql = "INSERT INTO usuarios (id, primerNombre, segundoNombre, primerApellido, segundoApellido, edad, telefono)VALUES (:id, :primerNmbre, :segundoNmbre, :primerApellido, :segundoApellido, :fecha_nacimiento, :telefono)";
-            $con = $this->getConexion() ->prepare($sql);                     
-            $con->bindParam(':id', $id);
-            $con->bindParam(':primerNmbre', $primerNombre);
-            $con->bindParam(':segundoNmbre', $segundoNombre);   
-            $con->bindParam(':primerApellido', $primerApellido);
-            $con->bindParam(':segundoApellido', $segundoApellido);
-            $con->bindParam(':fecha_nacimiento', $fecha_nacimiento); 
-            $con->bindParam(':telefono', $telefono);
+    public function Crear()
+    {
+        try {
+            $sql = "INSERT INTO usuarios (primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, fecha_nacimiento, telefono)VALUES (primer_nombre, segundo_nombre, primer_apellido,segundo_apellido,fecha_nacimiento, telefono)";
+            $con = $this->conn->prepare($sql);
+            $con->bindParam('primer_nombre', $this->primerNombre);
+            $con->bindParam('segundo_nombre', $this->segundoNombre);
+            $con->bindParam('primer_apellido', $this->primerApellido);
+            $con->bindParam('segundo_apellido', $this->segundoApellido);
+            $con->bindParam('fecha_nacimiento', $this->fecha_nacimiento);
+            $con->bindParam('telefono', $this->telefono);
             $con->execute();
             return true;
-    
-        }catch (PDOException $e) {
-            echo "Error:" . $e ->getMessage();
+        } catch (PDOException $e) {
+            echo "Error:" . $e->getMessage();
             return false;
         }
     }
